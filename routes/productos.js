3c4f4a8e8c4f6a5dadaf296b17fe84bfcc73bd02
mecolor123456-dev <mecolor123456@gmail.com>
2025-10-24 13:44:14 -0600

    Update productos.js

--- routes/productos.js
+++ routes/productos.js
@@ -1,30 +1,27 @@
 import express from "express";
 import pool from "../db.js";
-
 const router = express.Router();
 
-// Obtener todos los productos
+// Obtener productos
 router.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM productos ORDER BY id ASC");
     res.json(result.rows);
   } catch (err) {
-    console.error(err);
     res.status(500).json({ error: "Error al obtener productos" });
   }
 });
 
 // Agregar producto
 router.post("/", async (req, res) => {
-  const { nombre, categoria, precio, personalizaciones } = req.body;
+  const { nombre, categoria, precio, personalizaciones, imagen } = req.body;
   try {
     const result = await pool.query(
-      "INSERT INTO productos (nombre, categoria, precio, personalizaciones) VALUES ($1,$2,$3,$4) RETURNING *",
-      [nombre, categoria, precio, personalizaciones]
+      "INSERT INTO productos (nombre,categoria,precio,personalizaciones,imagen) VALUES ($1,$2,$3,$4,$5) RETURNING *",
+      [nombre, categoria, precio, personalizaciones, imagen]
     );
     res.json(result.rows[0]);
   } catch (err) {
-    console.error(err);
     res.status(500).json({ error: "Error al agregar producto" });
   }
 });
@@ -36,7 +33,6 @@ router.delete("/:id", async (req, res) => {
     await pool.query("DELETE FROM productos WHERE id=$1", [id]);
     res.json({ mensaje: "Producto eliminado" });
   } catch (err) {
-    console.error(err);
     res.status(500).json({ error: "Error al eliminar producto" });
   }
 });
