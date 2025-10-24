110f36934c0bf2bd32be367d731bfe56f1df6a1e
mecolor123456-dev <mecolor123456@gmail.com>
2025-10-24 12:49:01 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
-// 👇 PEGA AQUÍ TU URL DE RENDER
 const pool = new Pool({
-  connectionString: "postgresql://cafeteria_db_3wy0_user:eGXXbYcJn47Go7jRuSnRU9H9Hiz7bFZe@dpg-d3suuqfdiees73csljog-a.oregon-postgres.render.com/cafeteria_db_3wy0",
+  connectionString: process.env.DATABASE_URL, // Render DATABASE_URL
   ssl: {
     rejectUnauthorized: false
   }
