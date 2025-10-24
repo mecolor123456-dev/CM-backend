c5705d96bec85c510f620cb49e09a21a2f682315
mecolor123456-dev <mecolor123456@gmail.com>
2025-10-24 13:43:28 -0600

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,8 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Render DATABASE_URL
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
